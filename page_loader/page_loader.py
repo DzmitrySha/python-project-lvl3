7b2c048ad89a9cfeecf571972756aa51e848814d
DzmitrySha <ravenbydev@gmail.com>
2022-06-07 00:10:04 +0300

    modified page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,8 +1,15 @@
 # Downloader
 
+import os
 import requests
 
 
-def download(source_path: str, temp_path='var/tmp'):
-    r = requests.get(source_path)
-    return r.text
+def download(url: str, temp_folder='var/tmp'):
+    r = requests.get(url)
+    file_path = os.path.join(os.path.dirname(__file__),
+                             temp_folder, 'text_file')
+    with open(file_path, 'w') as file:
+        file.write(r.text)
+    with open(file_path, 'r') as file:
+        text = file.read()
+    return text
