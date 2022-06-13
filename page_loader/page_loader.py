8814598b74b3ce2c73cbf9db20113cb2893e304e
DzmitrySha <ravenbydev@gmail.com>
2022-06-13 23:43:36 +0300

    modified page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -6,30 +6,28 @@ import requests
 from urllib.parse import urlparse
 
 
-def strip_url(url: str) -> str:
-    parsed = urlparse(url)
-    scheme = "%s://" % parsed.scheme
-    return parsed.geturl().replace(scheme, '', 1)
+def strip_scheme(url: str) -> str:
+    parsed_url = urlparse(url)
+    scheme = f"{parsed_url.scheme}://"
+    return parsed_url.geturl().replace(scheme, '', 1)
 
 
 def to_string(url: str) -> str:
-    result = ''
-    for char in strip_url(url):
-        if not re.match('[a-zA-Z0-9]', char):
-            result += '-'
-        else:
-            result += char
+    indent = '-'
+    reg = '[a-zA-Z0-9]'
+    strip_url = strip_scheme(url)
 
-    return result + ".html"
+    result = "".join(char if re.match(reg, char) else indent
+                     for char in strip_url)
+
+    return result + ".html" if not os.path.splitext(url)[1] else result
 
 
 def download(url: str, temp_folder=''):
     r = requests.get(url)
-
     file_path = os.path.join(temp_folder, to_string(url))
 
     with open(file_path, 'w') as file:
         file.write(r.text)
-    # with open(file_path, 'r') as file:
-    #     text = file.read()
+
     return file_path
