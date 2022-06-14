149696ce9d1e39cd472d7c48233cd73030230600
DzmitrySha <ravenbydev@gmail.com>
2022-06-14 18:25:32 +0300

    added cli.py

---
+++ page_loader/cli.py
@@ -0,0 +1,16 @@
+# parsing command line
+
+import os
+import argparse
+
+
+def parse_args_():
+    parser = argparse.ArgumentParser(
+        prog='page_loader',
+        description="Downloads a page from the internet "
+                    "to the specified folder.")
+    parser.add_argument('source_url', type=str)
+    parser.add_argument('-o', '--output',
+                        default=os.getcwd())
+    args = parser.parse_args()
+    return args.source_url, args.output
