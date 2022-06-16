305c6180b65b7bf34709e6d8830f095440161e47
DzmitrySha <ravenbydev@gmail.com>
2022-06-16 09:56:18 +0300

    added test_url_to_filename

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -4,7 +4,7 @@ import os
 import pytest
 import requests
 from unittest.mock import Mock
-from page_loader.page_loader import download
+from page_loader.page_loader import download, url_to_filename
 
 FIXTURES_FOLDER = 'fixtures'
 
@@ -17,12 +17,17 @@ def text_html():
         return file.read()
 
 
+def test_url_to_filename():
+    assert url_to_filename('http://test.com') == 'test-com.html'
+    assert url_to_filename('https://site/test.com') == 'site-test-com.html'
+
+
 def test_request(requests_mock):
     requests_mock.get('https://test.com', text='data')
     assert 'data' == requests.get('https://test.com').text
 
 
-def test_save_file(text_html):
+def test_download(text_html):
     mock = Mock()
     mock.result = 'some pretty html code'
     assert mock.result == text_html
