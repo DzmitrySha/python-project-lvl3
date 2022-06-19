c03aa7bda4df5ba21a693d34b8966b0366c3ad52
DzmitrySha <ravenbydev@gmail.com>
2022-06-19 23:29:50 +0300

    modified test_page_loader.py

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -11,8 +11,9 @@ FIXTURES_FOLDER = 'fixtures'
 
 @pytest.fixture(autouse=True)
 def text_html():
+    """Return """
     result_path = os.path.join(os.path.dirname(__file__),
-                               FIXTURES_FOLDER, 'page_text')
+                               FIXTURES_FOLDER, 'text_html')
     with open(result_path) as file:
         return file.read()
 
@@ -33,13 +34,10 @@ def test_url_to_filename(urls):
     assert url_to_filename(https_url) == 'site-test-com.html'
 
 
-# def test_request(requests_mock):
-#     requests_mock.get('https://test.com', text='data')
-#     data = requests.get('https://test.com').text
-#     assert 'data' == data
-#
-#
-# def test_download(text_html):
-#     mock = Mock()
-#     mock.result = 'some pretty html code'
-#     assert mock.result == text_html
+def test_request(requests_mock):
+    requests_mock.get('https://test.com', text='data')
+    data = requests.get('https://test.com').text
+    assert 'data' == data
+
+
+# def test_download(urls):
