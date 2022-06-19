956cb74a01a365f18b5af0569366e32b5c7e26f1
DzmitrySha <ravenbydev@gmail.com>
2022-06-19 23:29:40 +0300

    modified page_loader.py

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -6,19 +6,13 @@ import requests
 from urllib.parse import urlparse
 
 
-def cut_url_scheme(url: str) -> str:
-    """Cut scheme ('http://' or 'https://') from url."""
-    parsed_url = urlparse(url)
-    scheme = f"{parsed_url.scheme}://"
-    return parsed_url.geturl().replace(scheme, '', 1)
-
-
 def url_to_filename(url: str) -> str:
     """Make file-name.html from url."""
     indent = '-'
     mask = '[a-zA-Z0-9]'
     ext = ".html"
-    strip_url = cut_url_scheme(url)
+    url_scheme = f"{urlparse(url).scheme}://"
+    strip_url = url.replace(url_scheme, '', 1)
     result = "".join(char if re.match(mask, char) else indent
                      for char in strip_url)
     return result + ext if os.path.splitext(url)[1] != ext else result
@@ -32,8 +26,10 @@ def write_to_file(file_path, text):
 
 def download(url: str, temp_folder='') -> str:
     """Download html page, save to exist specified folder."""
-    response = requests.get(url)
+    response_text = requests.get(url).text
+
     filename = url_to_filename(url)
     file_path = os.path.join(temp_folder, filename)
-    write_to_file(file_path, response.text)
+
+    write_to_file(file_path, response_text)
     return file_path
