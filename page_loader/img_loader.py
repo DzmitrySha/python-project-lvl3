77c04985225ba9991ef22413f7982d20ab897651
DzmitrySha <ravenbydev@gmail.com>
2022-06-25 00:17:59 +0300

    modified img_loader.py

--- page_loader/img_loader.py
+++ page_loader/img_loader.py
@@ -1,8 +1,17 @@
 """Image loader module."""
 
+import os
 from bs4 import BeautifulSoup
+
+
+def make_dir(url, temp_folder=''):
+    os.mkdir(url)
+    pass
 
 
 def img_download(url, temp_folder=''):
     soup = BeautifulSoup(url, 'html.parser')
+    img_links = soup.find_all('a')
+    for link in img_links:
+        print(link.get('href'))
     print(soup.prettify())
