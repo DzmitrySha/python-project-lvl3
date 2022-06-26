711e458399c843ac2e68ab8b0fe29f349e75eeea
DzmitrySha <ravenbydev@gmail.com>
2022-06-26 23:46:45 +0300

    modified img_loader.py

--- page_loader/img_loader.py
+++ page_loader/img_loader.py
@@ -1,7 +1,9 @@
 """Image loader module."""
 
 import os
+import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
 def make_dir(url, temp_folder=''):
@@ -11,7 +13,13 @@ def make_dir(url, temp_folder=''):
 
 def img_download(url, temp_folder=''):
     soup = BeautifulSoup(url, 'html.parser')
-    img_links = soup.find_all('a')
-    for link in img_links:
-        print(link.get('href'))
-    print(soup.prettify())
+    img_urls = soup.find_all('a')
+    domain_name = urlparse(url).netloc
+
+    for img_url in img_urls:
+        if domain_name in img_url:
+            r = requests.get(img_url)
+            with open(img_url, "wb") as img:
+                img.write(r.content)
+    #         print(link.get('href'))
+    # print(soup.prettify())
