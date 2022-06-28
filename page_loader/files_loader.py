89b77d2c983e15b587edac4eb92502cce80541ce
DzmitrySha <ravenbydev@gmail.com>
2022-06-28 20:36:29 +0300

    modified files_loader.py

--- page_loader/files_loader.py
+++ page_loader/files_loader.py
@@ -4,22 +4,24 @@ import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from page_loader.naming import make_name
 
 
-def make_dir(url, temp_folder=''):
-    os.mkdir(url)
-    pass
+def make_dir(url):
+    temp_folder = make_name(url, ext="_files")
+    os.mkdir(temp_folder)
+    return temp_folder
 
 
-def files_download(url, temp_folder=''):
+def files_download(url):
     soup = BeautifulSoup(url, 'html.parser')
     files_urls = soup.find_all('a')
     domain_name = urlparse(url).netloc
 
-    for img_url in files_urls:
-        if domain_name in img_url:
-            r = requests.get(img_url)
-            with open(img_url, "wb") as img:
-                img.write(r.content)
+    for file_url in files_urls:
+        if domain_name in (file_url, ""):
+            r = requests.get(file_url)
+            with open(file_url, "wb") as file:
+                file.write(r.content)
     #         print(link.get('href'))
     # print(soup.prettify())
