d65aa30e858d99342577a512306d5c107cb467b3
DzmitrySha <ravenbydev@gmail.com>
2022-07-03 17:03:47 +0300

    added and modified processes.py

--- page_loader/processes.py
+++ page_loader/processes.py
@@ -1,8 +1,15 @@
-"""Naming module"""
+"""Processes module."""
 
 import os
 import re
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+from urllib.request import urlopen
+
+
+def html_parser(url: str) -> tuple:
+    soup = BeautifulSoup(urlopen(url), 'html.parser')
+    return soup, soup.prettify()
 
 
 def clearing_(url: str) -> str:
@@ -19,8 +26,14 @@ def make_name(url: str, ext='') -> str:
     mask = '[a-zA-Z0-9]'
     clear_url = clearing_(url)
     extension = os.path.splitext(clear_url)[1]
-    if extension:
+    if extension == ext:
         clear_url = clear_url.replace(extension, '')
     indent_name = "".join(char if re.match(mask, char) else indent
                           for char in clear_url)
     return indent_name + ext if ext else indent_name
+
+
+def write_to_file(file_path: str, text: str):
+    """Write text to file."""
+    with open(file_path, 'w') as file:
+        file.write(text)
