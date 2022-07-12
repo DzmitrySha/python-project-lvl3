40011c979b1f93383b974e03fd33bb42a00632a9
DzmitrySha <ravenbydev@gmail.com>
2022-07-12 20:19:20 +0300

    modified downloader.py processes.py

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -1,21 +1,30 @@
 """Image loader module."""
 
 
-from page_loader.processes import (make_soup, make_dir_path,
-                                   write_html_file, write_bin_files)
+from page_loader.processes import (make_soup, make_dir_path, create_dir,
+                                   make_html_file_path, write_html_file,
+                                   write_files)
+
+tags = {
+    "img": "src",
+    "link": "href",
+    # "script": "src",
+}
 
 
 def page_download(url: str, temp_folder=""):
-    """Downloads and saves html page with images to exist specified folder"""
+    """Downloads and saves html page
+    with local resources
+    to exist specified folder"""
 
-    # создаем директорию для сохранения файлов
-    dir_path = make_dir_path(url, temp_folder)
-    # создаем объект Beautifulsoup
     soup = make_soup(url)
-    # создаем объект Beautifulsoup
-    write_bin_files(soup, url, "img", "src", dir_path)
-    write_bin_files(soup, url, "link", "href", dir_path)
-    # write_bin_files(soup, url, "script", "src", dir_path)
-    html_file_path = write_html_file(soup, url, temp_folder)
+    dir_path = make_dir_path(url, temp_folder)
+    create_dir(dir_path)
+    # находим, скачиваем и записываем ресурсы со страницы (по тегу и аттрибуту)
+    for tag, attr in tags.items():
+        write_files(soup, url, tag, attr, dir_path)
+
+    html_file_path = make_html_file_path(url, temp_folder)
+    write_html_file(soup, html_file_path)
 
     return html_file_path
--- page_loader/processes.py
+++ page_loader/processes.py
@@ -9,89 +9,89 @@ from bs4 import BeautifulSoup
 
 
 def make_soup(url: str) -> BeautifulSoup:
+    """Make soup object from url"""
     soup = BeautifulSoup(urlopen(url), 'html.parser')
     return soup
 
 
-def clearing_url(url: str) -> str:
-    url_parse = urlparse(url)
-    url_netloc = url_parse.netloc
-    url_path = url_parse.path
-    clear_url = url_netloc + url_path
-    return clear_url
-
-
 def make_name(url: str, ext='') -> str:
-    """Make indent-name with extension from url."""
+    """Make indent-name from url with specified extension."""
     indent = '-'
     mask = '[a-zA-Z0-9]'
-    clear_url = clearing_url(url)
+    parsed_url = urlparse(url)
+    clear_url = parsed_url.netloc + parsed_url.path
     extension = os.path.splitext(clear_url)[1]
     if extension == ext:
         clear_url = clear_url.replace(extension, '')
-    indent_name = "".join(char if re.match(mask, char) else indent
-                          for char in clear_url)
-    return indent_name + ext if ext else indent_name
+    name = "".join(char if re.match(mask, char) else indent
+                   for char in clear_url)
+    return name + ext if ext else name
 
 
 def make_dir_path(url: str, temp_folder='') -> str:
-    # директория и путь для хранения файлов
+    """Make directory path"""
     dir_name = make_name(url, ext="_files")
-    dir_path = os.path.join(temp_folder, dir_name)
-    # если директории не существует, то создаём её
+    if temp_folder:
+        dir_path = os.path.join(temp_folder, dir_name)
+        return dir_path
+    return dir_name
+
+
+def create_dir(dir_path: str):
+    """Create directory if it not exists"""
     if not os.path.exists(dir_path):
         os.mkdir(dir_path)
-    return dir_path
 
 
-def write_html_file(soup: BeautifulSoup, url: str, temp_folder='') -> str:
-    # имя html-файла
+def make_html_file_path(url: str, temp_folder='') -> str:
+    """Make html file path"""
     html_file_name = make_name(url, ext=".html")
-    # путь к сохранению html-файла
     html_file_path = os.path.join(temp_folder, html_file_name)
-    # сохранение html-файла с измененными ссылками на файлы картинок
+    return html_file_path
+
+
+def write_html_file(soup: BeautifulSoup, html_file_path) -> str:
+    """Write html-content to file"""
     with open(html_file_path, 'w') as file:
         file.write(soup.prettify())
     return html_file_path
 
 
-def write_source_file(source_url, local_path):
-    """Write source content to file"""
-    # считываем и записываем контент, содержащийся в картинке в файл
+def write_bin_file(source_url, local_path):
+    """Write binary source content to file"""
     req = requests.get(source_url)
     with open(local_path, "wb") as file:
         file.write(req.content)
 
 
-def write_bin_files(soup: BeautifulSoup,
-                    url: str,
-                    tags: str,
-                    attr: str,
-                    dir_path: str):
-    # собираем все строки с тегом img из объекта soup (html файла)
-    source_tags = soup.find_all(tags)
+def write_files(soup: BeautifulSoup,
+                url: str,
+                tag: str,
+                attr: str,
+                dir_path: str):
     # выделяем из url доменное имя
     domain_name = urlparse(url).netloc
+    # собираем список строк с тегами tags из объекта soup
+    list_tags = soup.find_all(tag)
 
-    for tag in source_tags:
-        # из каждого тега img берём аттрибут src
-        source_url = tag.get(attr)
-        source_domain_name = urlparse(source_url).netloc
+    for line in list_tags:
+        # для каждой строки из списка берём значение его аттрибута
+        line_url = line.get(attr)
+        line_domain_name = urlparse(line_url).netloc
 
         # если src содержит доменное имя или не содержит его вообще,
         # то скачиваем файл
-        if not source_domain_name or domain_name in source_domain_name:
-            # если ссылка на файл не содержит схему и доменное имя,
-            # добавляем их в ссылку
-            if not source_domain_name:
-                source_url = urljoin(url, source_url)
+        if domain_name in line_domain_name or not line_domain_name:
+            # формируем ссылку на файл, если она не содержит схему и домен
+            if not line_domain_name:
+                line_url = urljoin(url, line_url)
 
             # формируем имя файла и локальный путь к нему
-            source_name = make_name(source_url, os.path.splitext(source_url)[1])
+            source_name = make_name(line_url, os.path.splitext(line_url)[1])
             local_path = os.path.join(dir_path, source_name)
 
-            write_source_file(source_url, local_path)
+            write_bin_file(line_url, local_path)
 
             # заменяем ссылки на локальные пути к файлам
-            for source in source_tags:
-                source[attr] = source[attr].replace(source_url, local_path)
+            # for source in list_tags:
+            line[attr] = line[attr].replace(line_url, local_path)
