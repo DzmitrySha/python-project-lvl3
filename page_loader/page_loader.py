ff9d3875670b1a74a347b2acd2ef3f6d32799208
DzmitrySha <ravenbydev@gmail.com>
2022-07-15 00:10:09 +0300

    delete unused files

--- page_loader/page_loader.py
+++
@@ -1,9 +0,0 @@
-"""Page loader - main module."""
-
-from page_loader.downloader import page_download
-
-
-def download(url: str, temp_folder=''):
-    """Start download."""
-    html_path = page_download(url, temp_folder)
-    return html_path
