26ae295f21277f730f5d03603ff11be1a8d0e776
DzmitrySha <ravenbydev@gmail.com>
2022-07-20 10:27:07 +0300

    modified test in test_download_html.py

--- tests/test_downloaders/test_download_html.py
+++ tests/test_downloaders/test_download_html.py
@@ -1,6 +1,5 @@
 """Test HTML downloader"""
 
-import os
 import pytest
 import requests
 import tempfile
@@ -10,26 +9,18 @@ from page_loader.download_html import html_download
 DOWNLOADER = 'download_html'
 
 
-# @pytest.mark.asyncio
-# async def test_html_download(before_html, result_html,
-#                              result_render, requests_mock):
-#
-#     with open(before_html, 'r') as file:
-#         soup = BeautifulSoup(file, "html.parser")
-#
-#     requests_mock.get("https://test.com", text=str(soup))
-#     data_before = requests.get("https://test.com", before_html).text
-#     data_after = requests.get("https://test.com", result_html).text
-#
-#     with tempfile.TemporaryDirectory() as tmpdirname:
-#         path_to_file = html_download(soup, "https://test.com/", tmpdirname)
-#
-#         with open(path_to_file, 'r') as file:
-#             data_after_func = file.read()
-#
-#         assert data_after == data_after_func
+@pytest.mark.asyncio
+async def test_html_download(before_html, result_html, requests_mock):
 
+    with open(before_html, 'r') as file:
+        soup = BeautifulSoup(file, "html.parser")
 
-    # requests_mock.get('https://test.com', text='data')
-    # data = requests.get('https://test.com').text
-    # assert 'data' == data
+    requests_mock.get("https://ru.hexlet.io", text=soup.prettify())
+    data_after_mock = requests.get("https://ru.hexlet.io", result_html).text
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        path_to_file = html_download(soup, "https://ru.hexlet.io", tmpdirname)
+        with open(path_to_file, 'r') as file:
+            data_after_func = file.read()
+
+        assert data_after_mock == data_after_func
