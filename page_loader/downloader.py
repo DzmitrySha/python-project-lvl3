47fa1adabadf4a44b67b6293e3feadc89e470941
DzmitrySha <ravenbydev@gmail.com>
2022-07-26 16:26:25 +0300

    add logging to downloader.py

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -1,18 +1,26 @@
 """Page loader - start module."""
 
+import os
+from page_loader import app_logger
 from page_loader.processes import make_soup
 from page_loader.download_sources import sources_download
 from page_loader.download_html import html_download
+
+logger = app_logger.get_logger(__name__)
 
 
 def page_download(url: str, temp_folder=""):
     """Downloads and saves html page
     with local resources
     to exist specified folder"""
+    logger.info(f"requested url: {url}")
+    logger.info(f"output path: {os.path.join(os.getcwd(), temp_folder)}")
 
     soup = make_soup(url)
+    html_file_path = html_download(soup, url, temp_folder)
+    logger.info(f"write html file: {html_file_path}")
+    logger.info(f"Downloading: ...")
 
     sources_download(soup, url, temp_folder)
-    html_file_path = html_download(soup, url, temp_folder)
 
-    return html_file_path
+    return f"Page was downloaded as: {html_file_path}"
