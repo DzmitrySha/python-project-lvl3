05b171be821ce1ec225484c157a0886d78b61ec5
DzmitrySha <ravenbydev@gmail.com>
2022-07-26 16:25:55 +0300

    mod app_logger.py

--- page_loader/app_logger.py
+++ page_loader/app_logger.py
@@ -4,21 +4,22 @@ import os
 import logging
 from page_loader.processes import create_dir
 
-_log_format = f"%(asctime)s [%(levelname)s]: (%(filename)s) - %(funcName)s(%(lineno)d) - %(message)s"
+_log_file_format = f"%(asctime)s [%(levelname)s]: (%(filename)s) - %(funcName)s(%(lineno)d) - %(message)s"
+_log_stream_format = f"[%(levelname)s]: %(message)s"
 
 
 def get_file_handler():
     create_dir(os.path.join(os.getcwd(), 'logs'))
     file_handler = logging.FileHandler("logs/app_log.log")
     file_handler.setLevel(logging.WARNING)
-    file_handler.setFormatter(logging.Formatter(_log_format))
+    file_handler.setFormatter(logging.Formatter(_log_file_format))
     return file_handler
 
 
 def get_stream_handler():
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(logging.Formatter(_log_format))
+    stream_handler.setFormatter(logging.Formatter(_log_stream_format))
     return stream_handler
 
 
