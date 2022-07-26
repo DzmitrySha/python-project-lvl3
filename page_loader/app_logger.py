af16af519680bdd897c1170baefc63a2ebc73a53
DzmitrySha <ravenbydev@gmail.com>
2022-07-26 16:10:07 +0300

    added logger module

---
+++ page_loader/app_logger.py
@@ -0,0 +1,30 @@
+"""Logging module"""
+
+import os
+import logging
+from page_loader.processes import create_dir
+
+_log_format = f"%(asctime)s [%(levelname)s]: (%(filename)s) - %(funcName)s(%(lineno)d) - %(message)s"
+
+
+def get_file_handler():
+    create_dir(os.path.join(os.getcwd(), 'logs'))
+    file_handler = logging.FileHandler("logs/app_log.log")
+    file_handler.setLevel(logging.WARNING)
+    file_handler.setFormatter(logging.Formatter(_log_format))
+    return file_handler
+
+
+def get_stream_handler():
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(logging.Formatter(_log_format))
+    return stream_handler
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(get_stream_handler())
+    logger.addHandler(get_file_handler())
+    return logger
