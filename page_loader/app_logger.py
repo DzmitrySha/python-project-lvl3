9bd91ac7e836ac54c5b5a2ee2d3dd9ce5db73008
DzmitrySha <ravenbydev@gmail.com>
2022-08-14 17:23:54 +0300

    modified loggers in app_logger.py

--- page_loader/app_logger.py
+++ page_loader/app_logger.py
@@ -2,31 +2,51 @@
 
 import logging
 
-_log_file_format = "%(asctime)s [%(levelname)s]: " \
-                   "(%(filename)s) - %(funcName)s(%(lineno)d) - %(message)s"
-_log_stream_format = "[%(levelname)s]: %(message)s"
 
+class CustomFormatter(logging.Formatter):
 
-def get_file_handler():
-    """Make file handler"""
-    file_handler = logging.FileHandler("app_log.log")
-    file_handler.setLevel(logging.WARNING)
-    file_handler.setFormatter(logging.Formatter(_log_file_format))
-    return file_handler
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    # format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s " \
+    #          "(%(filename)s:%(lineno)d)"
 
+    _file_format = "(%(asctime)s) [%(levelname)s]: " \
+                   "%(name)s:%(filename)s:%(lineno)d %(message)s"
 
-def get_stream_handler():
-    """Make stream handler"""
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(logging.Formatter(_log_stream_format))
-    return stream_handler
+    _stream_format = "[%(levelname)s] %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + _file_format + reset,
+        logging.INFO: grey + _stream_format + reset,
+        logging.WARNING: yellow + _file_format + reset,
+        logging.ERROR: red + _stream_format + reset,
+        logging.CRITICAL: bold_red + _file_format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
 
 
 def get_logger(name):
     """Make logger"""
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(CustomFormatter())
+    #
+    with open('page_loader.log', 'w'):
+        pass
+    file_handler = logging.FileHandler("page_loader.log")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(CustomFormatter())
+
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    logger.addHandler(get_file_handler())
-    logger.addHandler(get_stream_handler())
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
     return logger
