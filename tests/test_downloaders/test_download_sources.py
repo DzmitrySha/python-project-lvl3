dd36532bf1fa94ac98539d354f1d8beed5f2e449
DzmitrySha <ravenbydev@gmail.com>
2022-09-03 21:53:52 +0300

    modified test download sources No Mock

--- tests/test_downloaders/test_download_sources.py
+++ tests/test_downloaders/test_download_sources.py
@@ -12,6 +12,7 @@ PAGE_URL = "https://ru.hexlet.io/courses"
 JPG_URL = "https://ru.hexlet.io/assets/professions/python.jpg"
 CSS_URL = "https://ru.hexlet.io/assets/application.css"
 JS_URL = "https://ru.hexlet.io/packs/js/runtime.js"
+MOCK_URL = "https://cdn2.site.com/blog/assets/style.css"
 
 DIR_NAME = "ru-hexlet-io_files"
 FILENAMES = {
@@ -28,26 +29,31 @@ def read_file(path: str, flag='r'):
 
 
 @pytest.mark.asyncio
-async def test_sources_download(before_html_path, jpg_file_path,
-                                css_file_path, js_file_path,
-                                result_html_path, urls,
-                                requests_mock):
-    before_html_mock = read_file(before_html_path)
-    result_html_mock = read_file(result_html_path)
-    jpg_mock = read_file(jpg_file_path, 'rb')
-    css_mock = read_file(css_file_path)
-    js_mock = read_file(js_file_path, 'rb')
+async def test_sources_download(
+        before_html_path, jpg_file_path,
+        css_file_path, js_file_path,
+        result_html_path, urls,
+        requests_mock
+):
+    before_html_content = read_file(before_html_path)
+    result_html_content = read_file(result_html_path)
+    jpg_content = read_file(jpg_file_path, 'rb')
+    css_content = read_file(css_file_path)
+    js_content = read_file(js_file_path, 'rb')
+    mock_content = 'not_exist'
 
-    requests_mock.get(HTML_MAIN_URL, text=result_html_mock)
-    requests_mock.get(PAGE_URL, text=before_html_mock)
-    requests_mock.get(JPG_URL, content=jpg_mock)
-    requests_mock.get(CSS_URL, text=css_mock)
-    requests_mock.get(JS_URL, content=js_mock)
+    requests_mock.get(HTML_MAIN_URL, text=result_html_content)
+    requests_mock.get(PAGE_URL, text=before_html_content)
+    requests_mock.get(JPG_URL, content=jpg_content)
+    requests_mock.get(CSS_URL, text=css_content)
+    requests_mock.get(JS_URL, content=js_content)
+    requests_mock.get(MOCK_URL, text=mock_content)
 
-    soup = BeautifulSoup(before_html_mock, "html.parser")
+    soup = BeautifulSoup(before_html_content, "html.parser")
 
     with tempfile.TemporaryDirectory() as tmpdirname:
         sources_download(soup, HTML_MAIN_URL, tmpdirname)
         for file_name in FILENAMES.values():
             file_path = os.path.join(DIR_NAME, file_name)
             assert os.path.exists(os.path.join(tmpdirname, file_path))
+        assert not os.path.exists(os.path.join(tmpdirname, 'not_exist'))
