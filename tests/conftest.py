0aaff4183b4b66b04d724836071fb8586bd76681
DzmitrySha <ravenbydev@gmail.com>
2022-09-27 23:39:58 +0300

    modified test_downloader.py and fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,69 +1,42 @@
 """fixtures"""
 
 import os
-import asyncio
 import pytest
 
 FIXTURES_FOLDER = 'fixtures'
 EXPECTED_FOLDER = 'fixtures/expected'
 EXPECTED_FILES_FOLDER = 'fixtures/expected/page-loader-hexlet-repl-co_files'
+FILES_PATHS = ['page-loader-hexlet-repl-co-courses.html',
+               'page-loader-hexlet-repl-co-assets-professions-python.jpg',
+               'page-loader-hexlet-repl-co-assets-application.css',
+               'page-loader-hexlet-repl-co-script.js'
+               ]
 
 
-@pytest.fixture(scope="module")
-def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
+# @pytest.mark.parametrize()
+# def file_paths():
+#     return os.path.join(
+#         os.path.dirname(__file__), EXPECTED_FILES_FOLDER, FILES_PATHS)
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def before_html_path():
     return os.path.join(os.path.dirname(__file__), FIXTURES_FOLDER,
                         'page-loader-hexlet-repl-co.html'
                         )
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def result_html_path():
     return os.path.join(os.path.dirname(__file__), EXPECTED_FOLDER,
                         'page-loader-hexlet-repl-co.html'
                         )
 
 
-@pytest.fixture(scope="session")
-def html_file_path():
-    return os.path.join(
-        os.path.dirname(__file__), EXPECTED_FILES_FOLDER,
-        'page-loader-hexlet-repl-co-courses.html'
-    )
-
-
-@pytest.fixture(scope="session")
-def jpg_file_path():
-    return os.path.join(
-        os.path.dirname(__file__), EXPECTED_FILES_FOLDER,
-        'page-loader-hexlet-repl-co-assets-professions-python.jpg'
-    )
-
-
-@pytest.fixture(scope="session")
-def css_file_path():
-    return os.path.join(os.path.dirname(__file__), EXPECTED_FILES_FOLDER,
-                        'page-loader-hexlet-repl-co-assets-application.css'
-                        )
-
-
-@pytest.fixture(scope="session")
-def js_file_path():
-    return os.path.join(os.path.dirname(__file__), EXPECTED_FILES_FOLDER,
-                        'page-loader-hexlet-repl-co-script.js'
-                        )
-
-
-@pytest.fixture(scope="session")
+@pytest.fixture
 def urls():
     urls = {
-        'https_url': "https://page-loader.hexlet.repl.co",
+        'ok_url': "https://page-loader.hexlet.repl.co",
         'bad_url': 'site.com',
     }
     return urls
--- tests/test_downloaders/test_downloader.py
+++ tests/test_downloaders/test_downloader.py
@@ -1,28 +1,140 @@
 """Test downloader"""
 
-# import pytest
+import os
+import pytest
 # import requests
-# import tempfile
-# from bs4 import BeautifulSoup
-# from page_loader.downloader import download
-#
+import tempfile
+from bs4 import BeautifulSoup
+from unittest.mock import Mock
+from page_loader import prepare_data
+from page_loader.prepare_data import make_name, make_soup, prepare_resources
+from page_loader.in_out import create_dir
 
-# @pytest.mark.asyncio
-# async def test_download(
-#         before_html_path,
-#         result_html_path,
-#         requests_mock,
-#         urls
-# ):
-#     with open(before_html_path, 'r') as file:
-#         soup = BeautifulSoup(file, "html.parser")
+HTML_URL = "/courses"
+JPG_URL = "/assets/professions/nodejs.png"
+CSS_URL = "/assets/application.css"
+JS_URL = "/script.js"
+# MOCK_URL = "https://cdn2.page-loader.hexlet.repl.co/blog/assets/style.css"
+
+FILENAMES = {
+    "html_mock": "page-loader-hexlet-repl-co-courses.html",
+    "jpg_mock": "page-loader-hexlet-repl-co-assets-professions-nodejs.png",
+    "css_mock": "page-loader-hexlet-repl-co-assets-application.css",
+    "js_mock": "page-loader-hexlet-repl-co-script.js",
+}
+
+DIR_NAME = "page-loader-hexlet-repl-co_files"
+
+PREPARED_RESOURCES = (
+    [('/assets/application.css', 'page-loader-hexlet-repl-co_files/page-loader-hexlet-repl-co-assets-application.css'),
+     ('/courses', 'page-loader-hexlet-repl-co_files/page-loader-hexlet-repl-co-courses.html'),
+     ('/assets/professions/nodejs.png', 'page-loader-hexlet-repl-co_files/page-loader-hexlet-repl-co-assets-professions-nodejs.png'),
+     ('/script.js', 'page-loader-hexlet-repl-co_files/page-loader-hexlet-repl-co-script.js')
+     ],
+    'page-loader-hexlet-repl-co.html'
+)
+
+
+def read_file(path: str, flag='r'):
+    with open(path, flag) as file:
+        return file.read()
+
+
+def test_prepare_resources(before_html_path, urls):
+
+    soup = BeautifulSoup(read_file(before_html_path), "html.parser")
+    prepare_data.make_soup = Mock(return_value=soup)
+    prepare_data.write_to_file = Mock()
+
+    prepared_resources = prepare_resources(urls['ok_url'])
+    assert prepared_resources == PREPARED_RESOURCES
+
+
+def test_make_soup(before_html_path, urls, requests_mock):
+    requests_mock.get(urls['ok_url'], text='')
+    soup = make_soup(urls['ok_url'])
+    assert type(soup) == BeautifulSoup
+
+
+def test_make_name(urls):
+    https_url = urls['ok_url']
+    assert make_name(https_url, ".html") == 'page-loader-hexlet-repl-co.html'
+    assert make_name(https_url, "_files") == 'page-loader-hexlet-repl-co_files'
+
+
+def test_create_dir():
+    with tempfile.TemporaryDirectory() as tempdir:
+        create_dir(tempdir)
+        assert os.path.exists(tempdir)
+        assert not os.path.exists("none")
+
+
+def test_make_soup_exceptions(urls):
+    with pytest.raises(Exception):
+        make_soup(urls['bad_url'])
+
+
+# def test_download(before_html_path, result_html_path,
+#                   requests_mock, urls
+#                   ):
 #
-#     requests_mock.get(urls['https_url'], text=soup.prettify())
-#     mock_data = requests.get(urls['https_url'], result_html_path).text
+#     soup = BeautifulSoup(read_file(before_html_path), "html.parser")
+#     requests_mock.get(urls['ok_url'], text=soup.prettify())
+#     mock_data = requests.get(urls['ok_url'], result_html_path).text
+#     # print(mock_data)
+#     html_file_content = read_file(result_html_path)
 #
 #     with tempfile.TemporaryDirectory() as tmpdirname:
-#         html_file_path = download(urls['https_url'], tmpdirname)
-#         with open(html_file_path, 'r') as file:
-#             html_file_content = file.read()
+#         prepare_data.prepare_resources = Mock(return_value=PREPARED_RESOURCES)
+#         in_out.write_to_file = Mock()
+#
+#         print(html_file_content)
+#         html_file_path = downloader.download(urls['ok_url'], tmpdirname)[1]
 #
 #         assert mock_data == html_file_content
+
+
+# def test_download_resources(
+#         before_html_path, jpg_file_path,
+#         css_file_path, js_file_path,
+#         result_html_path, html_file_path,
+#         urls, requests_mock
+# ):
+#     before_html_content = read_file(before_html_path)
+#     result_html_content = read_file(result_html_path)
+#     html_content = read_file(html_file_path)
+#     jpg_content = read_file(jpg_file_path, 'rb')
+#     css_content = read_file(css_file_path)
+#     js_content = read_file(js_file_path, 'rb')
+#     mock_content = 'not_exist'
+#
+#     requests_mock.get(urls['ok_url'], text=result_html_content)
+#     requests_mock.get(HTML_URL, text=html_content)
+#     requests_mock.get(JPG_URL, content=jpg_content)
+#     requests_mock.get(CSS_URL, text=css_content)
+#     requests_mock.get(JS_URL, content=js_content)
+#     requests_mock.get(MOCK_URL, text=mock_content)
+#
+#     soup = BeautifulSoup(before_html_content, "html.parser")
+#
+#     with tempfile.TemporaryDirectory() as tmpdirname:
+#         downloader.download(urls['ok_url'], tmpdirname)
+#         dir_path = os.path.join(tmpdirname, DIR_NAME)
+#
+#         for file_name in FILENAMES.values():
+#             file_path = os.path.join(tmpdirname, DIR_NAME, file_name)
+#             assert os.path.exists(file_path)
+#
+#         with open(os.path.join(dir_path, FILENAMES["html_mock"])) as f:
+#             assert f.read() == html_content
+#
+#         with open(os.path.join(dir_path, FILENAMES["jpg_mock"]), 'rb') as f:
+#             assert f.read() == jpg_content
+#
+#         with open(os.path.join(dir_path, FILENAMES["css_mock"])) as f:
+#             assert f.read() == css_content
+#
+#         with open(os.path.join(dir_path, FILENAMES["js_mock"]), 'rb') as f:
+#             assert f.read() == js_content
+#
+#         assert not os.path.exists(os.path.join(tmpdirname, mock_content))
